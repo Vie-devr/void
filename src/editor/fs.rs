12e2a0884cae0a250735a01906e6adb0fc627a2c
Pavel Golubkin <vie.devr@gmail.com>
2023-03-11 20:10:06 +0100

    Now editor fs methods can only be accessed from editor module

--- src/editor/fs.rs
+++ src/editor/fs.rs
@@ -2,28 +2,28 @@ use rfd::FileDialog;
 use std::fs;
 
 impl super::Editor {
-	pub fn new_file(&mut self) {
+	pub(super) fn new_file(&mut self) {
 		self.pre_caret.clear();
 		self.selection.clear();
 		self.post_caret.clear();
 		self.opened_file = None;
 	}
 
-	pub fn open_file(&mut self) {
+	pub(super) fn open_file(&mut self) {
 		if let Some(path) = self.construct_file_dialog().pick_file() {
 			// Why sequence converting PathBuf to string is so long?
 			self.open_file_from_path(path.to_str().unwrap().to_string());
 		}
 	}
 
-	pub fn open_file_from_path(&mut self, path: String) {
+	pub(super) fn open_file_from_path(&mut self, path: String) {
 		if let Ok(data) = fs::read_to_string(path.clone()) {
 			self.opened_file = Some(path);
 			self.post_caret = data.replace("    ", "\t").chars().rev().collect();
 		}
 	}
 	
-	pub fn save_file(&mut self) {
+	pub(super) fn save_file(&mut self) {
 		if let Some(path) = &self.opened_file {
 			fs::write(path, self.get_text()).unwrap();
 		}
@@ -32,7 +32,7 @@ impl super::Editor {
 		}
 	}
 
-	pub fn save_file_as(&mut self) {
+	pub(super) fn save_file_as(&mut self) {
 		if let Some(path) = self.construct_file_dialog().save_file() {
 			// Why sequence converting PathBuf to string is so long?
 			self.opened_file = Some(path.to_str().unwrap().to_string());
