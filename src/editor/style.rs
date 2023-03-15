a753ff4dfb65094b3efb6bf223f02d0396e847de
Pavel Golubkin <vie.devr@gmail.com>
2023-03-15 03:13:36 +0100

    █

--- src/editor/style.rs
+++ src/editor/style.rs
@@ -15,17 +15,19 @@ pub struct EditorStyle {
 }
 
 impl EditorStyle {
-	pub fn new(text_padding: f32, line_spacing: f32, caret_width: f32, font: Font, font_size: u16, font_scale: f32, background: Color, text: Color, caret: Color) -> Self {
+	pub fn new(text_padding: f32, line_spacing: f32, caret_width: f32,
+				font: Font, font_size: u16, font_scale: f32,
+				background: Color, text: Color, caret: Color) -> Self {
 		Self {
 			text_params: TextParams{
 				font,
 				font_size,
 				font_scale,
-				font_scale_aspect: 1.,
+				font_scale_aspect: 1.0,
 				rotation: 0.,
 				color: text,
 			},
-			dimensions: measure_text("T", Some(font), font_size, font_scale),
+			dimensions: measure_text("█", Some(font), font_size, font_scale),
 			text_padding,
 			line_spacing,
 			caret_width,
