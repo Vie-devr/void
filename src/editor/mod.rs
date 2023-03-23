23a1bdd9971ea0301108df43edc1cb6826bc4bd3
Pavel Golubkin <vie.devr@gmail.com>
2023-03-23 01:40:38 +0100

    Moved editor drawing to 'drawing' submodule

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -1,11 +1,11 @@
 mod style;
 mod fs;
 mod command_executor;
+mod drawing;
 
 pub use style::*;
 
 use macroquad::prelude::*;
-use macroquad::miniquad::CursorIcon;
 
 const HOLDING_KEY_EXECUTION_START_DELAY: f32 = 0.4;
 const HOLDING_KEY_EXECUTION_DELAY: f32 = 0.025;
@@ -49,8 +49,6 @@ impl Editor {
 	}
 
 	pub fn update(&mut self) {
-		self.update_cursor_icon();
-
 		// Pressed any key
 		if let Some(key) = get_last_key_pressed() {
 			// Reset key holding stuff
@@ -82,86 +80,6 @@ impl Editor {
 				}
 			}
 		}
-	}
-
-	pub fn draw(&self, drawing_rect: Rect) {
-		let line_nums_bar_width = self.style.measure_text(&format!(" {} ", self.lines.len())).width;
-
-		// Draw background
-		draw_rectangle(
-			drawing_rect.x + line_nums_bar_width,
-			drawing_rect.y,
-			drawing_rect.w - line_nums_bar_width,
-			drawing_rect.h,
-			self.style.background,
-		);
-
-		// Draw line numbers bar
-		draw_rectangle(
-			drawing_rect.x,
-			drawing_rect.y,
-			line_nums_bar_width,
-			drawing_rect.h,
-			self.style.line_nums_background,
-		);
-
-		let text = self.content_as_text().replace("\t", &" ".repeat(self.style.tab_size));
-
-		// Draw editor content
-		for (i, line) in text.lines().enumerate() {
-			let x = drawing_rect.x + self.style.text_padding + line_nums_bar_width;
-			let y = drawing_rect.y + self.style.text_padding
-				  + self.style.dimensions.height * (i + 1) as f32
-				  + self.style.line_spacing * i as f32;
-
-			// Draw line number
-			draw_text_ex(
-				// I am too lazy to make here real padding. just spaces.
-				&format!(" {} ", i + 1),
-				drawing_rect.x,
-				y,
-				self.style.line_nums_params,
-			);
-
-			// Draw line
-			draw_text_ex(
-				line,
-				x,
-				y,
-				self.style.text_params,
-			);
-
-			if self.caret_row() == i {
-				let x = x + self.style.dimensions.width * self.caret_col() as f32;
-				let c = self.content[self.caret_pos];
-
-				// Draw caret
-				draw_rectangle(
-					x,
-					y - self.style.dimensions.height,
-					self.style.dimensions.width,
-					self.style.dimensions.height,
-					self.style.text,
-				);
-
-				if !c.is_whitespace() {
-					let mut params = self.style.text_params;
-					params.color = self.style.background;
-
-					// Draw char over the caret
-					draw_text_ex(
-						&c.to_string(),
-						x,
-						y,
-						params,
-					);
-				}
-			}
-		}
-	}
-	
-	fn content_as_text(&self) -> String {
-		String::from_iter(self.content.iter())
 	}
 
 	fn caret_row(&self) -> usize {
@@ -196,20 +114,6 @@ impl Editor {
 				self.lines.push(Line::new(start, i));
 				start = i + 1;
 			}
-		}
-	}
-
-	fn update_cursor_icon(&self) {
-		let mouse_pos = mouse_position();
-		let context = unsafe { get_internal_gl().quad_context };
-
-		// Cursor is over the editor
-		if mouse_pos.0 > 0.1 && mouse_pos.0 < screen_width()
-		&& mouse_pos.1 > 0.1 && mouse_pos.1 < screen_height() {
-			context.set_mouse_cursor(CursorIcon::Text);
-		}
-		else {
-			context.set_mouse_cursor(CursorIcon::Default);
 		}
 	}
 }
